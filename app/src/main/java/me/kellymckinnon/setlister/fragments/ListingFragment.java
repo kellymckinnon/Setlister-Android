662a17f560b37516dbe550879758a1eb84c2dc0f
kellymckinnon <kellymckinnon@utexas.edu>
2019-08-11 19:08:07 -0700

    Clean up ListingFragment; fix fragment backstack bug
    The bug was that when you went back to the fragment, it would reload the list of setlists, but from the page you left off on, not the original page.

--- app/src/main/java/me/kellymckinnon/setlister/fragments/ListingFragment.java
+++ app/src/main/java/me/kellymckinnon/setlister/fragments/ListingFragment.java
@@ -9,24 +9,19 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.List;
 import me.kellymckinnon.setlister.R;
 import me.kellymckinnon.setlister.SetlisterConstants;
-import me.kellymckinnon.setlister.models.Set;
 import me.kellymckinnon.setlister.models.Setlist;
 import me.kellymckinnon.setlister.models.Setlists;
 import me.kellymckinnon.setlister.models.Show;
-import me.kellymckinnon.setlister.models.Song;
 import me.kellymckinnon.setlister.network.RetrofitClient;
 import me.kellymckinnon.setlister.network.SetlistFMService;
 import me.kellymckinnon.setlister.utils.Utility;
@@ -42,17 +37,18 @@ import retrofit2.Response;
  */
 public class ListingFragment extends Fragment {
 
-  private LinearLayoutManager mLinearLayoutManager;
+  // Passed in information about the artist we want to search setlists for
+  private String mArtistName;
+  private String mArtistId;
+
   private RecyclerView mRecyclerView;
-  private String mQuery;
   private TextView mNoShowsTextView;
   private ProgressBar mLoadingShowsProgressBar;
-  private int mNumPagesLoaded;
-  private int mFirstVisibleItemIndex, mVisibleItemCount, mTotalItemCount;
-  private boolean mIsLoading = false;
+
   private ShowAdapter mAdapter;
+  private int mNumPagesLoaded = 0;
+  private boolean mIsLoading = false;
   private int mNumSetlistPages; // Number of pages of setlists from API
-  private String mArtistId;
   private SetlistFMService mSetlistFMService;
   private OnSetlistSelectedListener mOnSetlistSelectedListener;
 
@@ -71,50 +67,50 @@ public class ListingFragment extends Fragment {
   }
 
   @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mSetlistFMService = RetrofitClient.getSetlistFMService();
+  }
+
+  @Override
   public View onCreateView(
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    mSetlistFMService = RetrofitClient.getSetlistFMService();
-
     View rootView = inflater.inflate(R.layout.fragment_listing, container, false);
-    mQuery = getArguments().getString(SetlisterConstants.EXTRA_ARTIST_NAME);
-    mArtistId = getArguments().getString(SetlisterConstants.EXTRA_ARTIST_ID);
     mNoShowsTextView = rootView.findViewById(R.id.no_shows);
     mLoadingShowsProgressBar = rootView.findViewById(R.id.loading_shows);
     mRecyclerView = rootView.findViewById(R.id.show_list);
-    DividerItemDecoration decoration =
-        new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
-    mRecyclerView.addItemDecoration(decoration);
-    mLinearLayoutManager = new LinearLayoutManager(getActivity());
+    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
 
-    mFirstVisibleItemIndex = 0;
-    mVisibleItemCount = 0;
-    mTotalItemCount = 0;
+    if (mArtistName == null) { // Fragment's first time being created
+      mArtistName = getArguments().getString(SetlisterConstants.EXTRA_ARTIST_NAME);
+      mArtistId = getArguments().getString(SetlisterConstants.EXTRA_ARTIST_ID);
+      mAdapter = new ShowAdapter(mOnSetlistSelectedListener);
+      getSetlists();
+    } else { // Fragment is being recreated
+      mRecyclerView.setVisibility(View.VISIBLE);
+      mLoadingShowsProgressBar.setVisibility(View.GONE);
+    }
 
-    mAdapter = new ShowAdapter(mOnSetlistSelectedListener);
     mRecyclerView.setAdapter(mAdapter);
     mRecyclerView.setHasFixedSize(true);
-    mRecyclerView.setLayoutManager(mLinearLayoutManager);
-    mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+    mRecyclerView.setLayoutManager(linearLayoutManager);
+    mRecyclerView.addItemDecoration(
+        new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
 
     /* Load new page(s) if the user scrolls to the end */
     mRecyclerView.addOnScrollListener(
         new RecyclerView.OnScrollListener() {
           @Override
           public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-
-            mVisibleItemCount = mLinearLayoutManager.getChildCount();
-            mTotalItemCount = mLinearLayoutManager.getItemCount();
-            mFirstVisibleItemIndex = mLinearLayoutManager.findFirstVisibleItemPosition();
-
             if (!mIsLoading && mNumPagesLoaded < mNumSetlistPages) {
-              if ((mVisibleItemCount + mFirstVisibleItemIndex) >= mTotalItemCount - 10) {
+              if ((linearLayoutManager.getChildCount()
+                      + linearLayoutManager.findFirstVisibleItemPosition())
+                  >= linearLayoutManager.getItemCount() - 10) {
                 getSetlists();
               }
             }
           }
         });
-
-    getSetlists();
 
     return rootView;
   }
@@ -166,11 +162,7 @@ public class ListingFragment extends Fragment {
 
             List<Setlist> setlists = response.body().getSetlist();
             for (Setlist setlist : setlists) {
-              Show show = getShowModel(setlist);
-              if (show == null) { // No setlists available for this show
-                continue;
-              }
-
+              Show show = new Show(setlist);
               mAdapter.add(show);
             }
 
@@ -195,70 +187,8 @@ public class ListingFragment extends Fragment {
     if (mArtistId != null) { // We have an MBID, use that
       mSetlistFMService.getSetlistsByArtistMbid(mArtistId, mNumPagesLoaded + 1).enqueue(callback);
     } else { // We have a plaintext name, use that
-      mSetlistFMService
-          .getSetlistsByArtistName(mQuery, mNumPagesLoaded + 1)
-          .enqueue(callback);
+      mSetlistFMService.getSetlistsByArtistName(mArtistName, mNumPagesLoaded + 1).enqueue(callback);
     }
-  }
-
-  /** Move data from JSON response into Show objects */
-  private Show getShowModel(Setlist setlist) {
-    String tour =
-        setlist.getTour() == null || setlist.getTour().getName().equals("No Tour Assigned")
-            ? ""
-            : setlist.getTour().getName();
-
-    String year = setlist.getEventDate().substring(6);
-    String day = setlist.getEventDate().substring(0, 2);
-    String month = setlist.getEventDate().substring(3, 5);
-    String date = month + "/" + day + "/" + year;
-
-    String venue;
-    try {
-      StringBuilder builder = new StringBuilder();
-      builder.append(setlist.getVenue().getName());
-      builder.append(", ");
-      builder.append(setlist.getVenue().getCity().getName());
-      builder.append(", ");
-      builder.append(setlist.getVenue().getCity().getStateCode());
-      builder.append(", ");
-      builder.append(setlist.getVenue().getCity().getCountry().getCode());
-
-      venue = builder.toString();
-    } catch (Exception e) {
-      venue = getString(R.string.unknown_venue);
-    }
-
-    String[] songs;
-    try {
-      List<Set> sets = setlist.getSets().getSet();
-      ArrayList<String> songList = new ArrayList<>();
-
-      // Combine all of the sets into one for viewing
-      for (Set set : sets) {
-        for (Song song : set.getSong()) {
-          if (!song.getName().isEmpty()) {
-            songList.add(song.getName());
-          }
-        }
-      }
-
-      songs = songList.toArray(new String[0]);
-    } catch (Exception e) {
-      // Usually, this just means there are no songs in the setlist, and "sets"
-      // is an empty string instead of an object.
-
-      e.printStackTrace();
-      return null;
-    }
-
-    return new Show.Builder()
-        .setBand(setlist.getArtist().getName())
-        .setTour(tour)
-        .setDate(date)
-        .setVenue(venue)
-        .setSongs(songs)
-        .createShow();
   }
 
   private void showNullState() {
--- app/src/main/java/me/kellymckinnon/setlister/models/Show.java
+++ app/src/main/java/me/kellymckinnon/setlister/models/Show.java
@@ -2,6 +2,8 @@ package me.kellymckinnon.setlister.models;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import java.util.ArrayList;
+import java.util.List;
 
 /** Model for a concert */
 public class Show implements Parcelable {
@@ -24,12 +26,44 @@ public class Show implements Parcelable {
   private String tour;
   private String[] songs;
 
-  public Show(String band, String venue, String date, String tour, String[] songs) {
-    this.band = band;
-    this.venue = venue;
-    this.date = date;
-    this.tour = tour;
-    this.songs = songs;
+  public Show(Setlist setlist) {
+    tour =
+        setlist.getTour() == null || setlist.getTour().getName().equals("No Tour Assigned")
+            ? ""
+            : setlist.getTour().getName();
+
+    String year = setlist.getEventDate().substring(6);
+    String day = setlist.getEventDate().substring(0, 2);
+    String month = setlist.getEventDate().substring(3, 5);
+    date = month + "/" + day + "/" + year;
+
+    try {
+      venue =
+          setlist.getVenue().getName()
+              + ", "
+              + setlist.getVenue().getCity().getName()
+              + ", "
+              + setlist.getVenue().getCity().getStateCode()
+              + ", "
+              + setlist.getVenue().getCity().getCountry().getCode();
+    } catch (Exception e) {
+      venue = "Unknown venue";
+    }
+
+    List<Set> sets = setlist.getSets() == null ? new ArrayList<>() : setlist.getSets().getSet();
+    ArrayList<String> songList = new ArrayList<>();
+
+    // Combine all of the sets into one for viewing
+    for (Set set : sets) {
+      for (Song song : set.getSong()) {
+        if (!song.getName().isEmpty()) {
+          songList.add(song.getName());
+        }
+      }
+    }
+
+    songs = songList.toArray(new String[0]);
+    band = setlist.getArtist().getName();
   }
 
   private Show(Parcel in) {
@@ -72,44 +106,5 @@ public class Show implements Parcelable {
 
   public String[] getSongs() {
     return songs;
-  }
-
-  public static class Builder {
-
-    private String band;
-    private String venue;
-    private String date;
-    private String tour;
-    private String[] songs;
-    private Parcel in;
-
-    public Builder setBand(String band) {
-      this.band = band;
-      return this;
-    }
-
-    public Builder setVenue(String venue) {
-      this.venue = venue;
-      return this;
-    }
-
-    public Builder setDate(String date) {
-      this.date = date;
-      return this;
-    }
-
-    public Builder setTour(String tour) {
-      this.tour = tour;
-      return this;
-    }
-
-    public Builder setSongs(String[] songs) {
-      this.songs = songs;
-      return this;
-    }
-
-    public Show createShow() {
-      return new Show(band, venue, date, tour, songs);
-    }
   }
 }
